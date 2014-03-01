8b3983e650e318585a9bd744df7584c19d4f039d
railsfactory-padmavathy <padmavathy@railsfactory.org>
2014-03-01 19:07:07 +0530

    default student set

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,8 +10,8 @@ class DashboardController < ApplicationController
     student = Student.first.assessments.last
    end
    assessments = Classroom.first.students.map {|a| a.assessments.last}.compact
-   sectionA = Student.where('classroom_id = ? && section_id = ?',1,1).map {|a| a.assessments.last}.compact 
-   sectionB = Student.where('classroom_id = ? && section_id = ?',1,2).map {|a| a.assessments.last}.compact
+   sectionA = Student.where('classroom_id = ? AND section_id = ?',1,1).map {|a| a.assessments.last}.compact 
+   sectionB = Student.where('classroom_id = ? AND section_id = ?',1,2).map {|a| a.assessments.last}.compact
    @class_room_result = (assessments.blank? )  ? [0, 0, 0] : [assessments.map(&:FA1).compact.reduce(:+)/assessments.map(&:FA1).count,assessments.map(&:FA2).compact.reduce(:+)/assessments.map(&:FA2).count,assessments.map(&:SA1).compact.reduce(:+)/assessments.map(&:SA1).count] 
    @sectionA =  (sectionA.blank? )  ? [0, 0, 0] : [ sectionA.map(&:FA1).compact.reduce(:+)/sectionA.map(&:FA1).count ,sectionA.map(&:FA2).compact.reduce(:+)/sectionA.map(&:FA2).count,sectionA.map(&:SA1).compact.reduce(:+)/sectionA.map(&:SA1).count]
    @sectionB = (sectionB.blank? )  ? [0, 0, 0] : [ sectionB.map(&:FA1).compact.reduce(:+)/sectionB.map(&:FA1).count ,sectionB.map(&:FA2).compact.reduce(:+)/sectionB.map(&:FA1).count,sectionB.map(&:SA1).compact.reduce(:+)/sectionB.map(&:SA1).count]
